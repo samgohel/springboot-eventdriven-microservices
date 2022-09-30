5e3af78b55118c946da6912196e0a97f6c91a4ad
Shambhu Gohel <gohelshambhu95@yahoo.com>
2022-09-30 15:58:44 +0530

    Consume Order Event In Services

---
+++ notification-service/src/main/java/com/sam/notificationservice/kafka/OrderConsumer.java
@@ -0,0 +1,17 @@
+package com.sam.notificationservice.kafka;
+
+import com.sam.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent event){
+        LOGGER.info(String.format("Order Event Received In Notification Service: %s", event.toString()));
+    }
+}
